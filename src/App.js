c7586f6ae3298d6fbc6cdac3d5a994f87609d770
izebiafe <afimiafidelis95@gmail.com>
2023-10-10 13:29:40 +0100

    Stage app in APp.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,10 @@ import WeatherDetails from './components/WeatherDetails';
 
 function App() {
   return (
-
     <Routes>
       <Route path="/" element={<Weather />} />
-      <Route
-        path="/city/:id"
-        element={<WeatherDetails />}
-      />
+      <Route path="/city/:id" element={<WeatherDetails />} />
     </Routes>
-
   );
 }
 
